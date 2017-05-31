1e2088c1c77754e09ff59b4a7ebd5cadc0c69e20
kristofkania33 <zyhkana25@wp.pl>
2017-05-31 20:04:30 +0200

    changed in js

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,13 @@ console.log(allNames);
 
 var newName = 'Marian';
 
-	if(allNames.indexOf(newName) === -1) {
+if(allNames.indexOf(newName) === -1) {
 		allNames.push(newName);
 		console.log("Now we have new table: " + allNames + " with length " + allNames.length);
-	}
-	else  {
+}
+else  {
 		console.log('The name already exist on table!!!');
-	}
+}
 
 
 
