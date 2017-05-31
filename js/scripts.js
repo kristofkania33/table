bf03878e910bbc8b5513cbd2d5cc8ee74761fde7
kristofkania33 <zyhkana25@wp.pl>
2017-05-31 19:55:15 +0200

    js changed

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,15 @@ var allNames = femaleNames.concat(maleNames);
 console.log(allNames);
 
 var newName = 'Marian';
-allNames.push('newName');
-var newOne = allNames.push(newName);
 
 	if(allNames.indexOf(newName) === -1) {
-		return "Now we have new table: " + allNames.push(newName) + " with lenght " + newOne;
+		allNames.push(newName);
+		console.log("Now we have new table: " + allNames + " with length " + allNames.length);
 	}
-	else if(allNames.indexOf(newName) !=== -1) {
-		return "The name can not be added because it is already	in table !!!";
+	else  {
+		console.log('The name already exist on table!!!');
 	}
+
+
+
+
